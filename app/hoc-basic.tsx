b0786eed48a4ec4abac7a8e07c4a7e8ba8fd5674
silviyaboteva <sbboteva@gmail.com>
2019-10-06 19:04:57 +0300

    added more examples for HOC concept

--- app/hoc-basic.tsx
+++ app/hoc-basic.tsx
@@ -16,6 +16,8 @@ function simpleHOC(WrappedComponent) {
 // Create a new component
 const NewComponent = simpleHOC(Hello);
 
+
+
 // Take in a component as argument WrappedComponent
 function withNameReact(WrappedComponent) {
   // And return a new anonymous component
@@ -28,10 +30,51 @@ function withNameReact(WrappedComponent) {
 
 const HelloReact = withNameReact(Hello);
 
+
+
+const Button = ({ value, increaseButton }) => <input type={'button'} value={value} onClick={increaseButton}/>;
+
+function clickableButton(WrappedComponent) {
+  class ClickableButton extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+       count: 0
+      }
+
+      this.handleIncrement = (event) => {
+        this.setState({ count: this.state.count + 1})
+      }
+    }
+    render() {
+      return (
+        <React.Fragment>
+          <h1>{this.state.count}</h1>
+          <WrappedComponent value={'Clickable button'} increaseButton={this.handleIncrement} {...this.props} />
+        </React.Fragment>
+      );
+    }
+  }
+
+  return ClickableButton;
+}
+
+const ClickableButton = clickableButton(Button);
+
 const App = () =>
   <div>
+    <p>Hello Component (Original)</p>
+    <Hello name={'Something'}/>
+    <p>HOC Component over Hello</p>
     <NewComponent name="CodeSandbox" />
+    <p>HOC Component over Hello</p>
     <HelloReact/>
+
+    <p>Button Component (Original)</p>
+    <Button/>
+    <p>HOC Component over Button</p>
+    <ClickableButton/>
   </div>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
