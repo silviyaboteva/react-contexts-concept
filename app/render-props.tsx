d85dca94e2e7a46ae379e6fc0ca688dec84138ff
silviyaboteva <sbboteva@gmail.com>
2019-10-06 20:09:40 +0300

    add demo for render props

--- app/render-props.tsx
+++ app/render-props.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+class Car extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <i className="fa fa-car" style={{ position: 'absolute', left: mouse.x, top: mouse.y, fontSize:'20px' }}></i>
+    );
+  }
+}
+
+class Circle extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <i className="fa fa-circle" style={{ position: 'absolute', left: mouse.x, top: mouse.y, fontSize:'20px', marginTop: '25px' }}></i>
+    );
+  }
+}
+
 // The <Mouse> component encapsulates the behavior we need...
 class Mouse extends React.Component {
   constructor(props) {
     super(props);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
     this.state = { x: 0, y: 0 };
   }
-
-  handleMouseMove(event) {
+  handleMouseMove = (event) => {
     this.setState({
       x: event.clientX,
       y: event.clientY
@@ -17,11 +33,16 @@ class Mouse extends React.Component {
   }
 
   render() {
+    
+    console.log(this.props);
     return (
       <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
 
-        {/* ...but how do we render something other than a <p>? */}
-        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+        {/*
+          Instead of providing a static representation of what <Mouse> renders,
+          use the `render` prop to dynamically determine what to render.
+        */}
+        {this.props.render(this.state)}
       </div>
     );
   }
@@ -32,7 +53,12 @@ class MouseTracker extends React.Component {
     return (
       <div>
         <h1>Move the mouse around!</h1>
-        <Mouse />
+        <Mouse render={mouseState => (
+          <React.Fragment>
+            <Car mouse={mouseState} />
+            <Circle mouse={mouseState} />
+          </React.Fragment>
+        )}/>
       </div>
     );
   }
